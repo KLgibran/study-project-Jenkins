2c099038e0d0fa2583b8881c159f0c4755c49b07
KLgibran <ce.ibrahim.ulker@gmail.com>
2023-06-18 21:55:56 +0000

    IAM user added

--- main .tf
+++ main .tf
@@ -30,7 +30,7 @@ resource "aws_instance" "managed_nodes" {
   instance_type = "t2.micro"
   key_name = "gibranAWS"  # change with your pem file
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
-  iam_instance_profile = "jenkins-project-profile-${var.user}" # we created this with jenkins server
+  iam_instance_profile = "jenkins-project-profile-200-${var.user}" # we created this with jenkins server
   tags = {
     Name = "ansible_${element(var.tags, count.index )}"
     stack = "ansible_project"
